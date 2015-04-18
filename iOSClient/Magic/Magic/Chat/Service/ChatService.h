939cebc7f337438661b179471356bbf584ed105a
cjn <noel.neng@gmail.com>
2015-04-18 15:04:02 +0800

    update pb

---
+++ iOSClient/Magic/Magic/Chat/Service/ChatService.h
@@ -0,0 +1,13 @@
+//
+//  ChatService.h
+//  Magic
+//
+//  Created by Teemo on 15/4/18.
+//  Copyright (c) 2015年 PIPICHENG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChatService : NSObject
+
+@end
---
+++ iOSClient/Magic/Magic/Chat/Service/ChatService.m
@@ -0,0 +1,13 @@
+//
+//  ChatService.m
+//  Magic
+//
+//  Created by Teemo on 15/4/18.
+//  Copyright (c) 2015年 PIPICHENG. All rights reserved.
+//
+
+#import "ChatService.h"
+
+@implementation ChatService
+
+@end
