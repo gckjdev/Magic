b535e73847c3a57f8edc698e45c48be12213df93
cjn <noel.neng@gmail.com>
2015-04-20 11:27:04 +0800

    update

---
+++ iOSClient/Magic/Magic/Common/Cdn/CreateFileInfo.h
@@ -0,0 +1,13 @@
+//
+//  CreateFileInfo.h
+//  Magic
+//
+//  Created by Teemo on 15/4/20.
+//  Copyright (c) 2015年 PIPICHENG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CreateFileInfo : NSObject
+
+@end
---
+++ iOSClient/Magic/Magic/Common/Cdn/CreateFileInfo.m
@@ -0,0 +1,13 @@
+//
+//  CreateFileInfo.m
+//  Magic
+//
+//  Created by Teemo on 15/4/20.
+//  Copyright (c) 2015年 PIPICHENG. All rights reserved.
+//
+
+#import "CreateFileInfo.h"
+
+@implementation CreateFileInfo
+
+@end
