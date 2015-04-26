89aedaffb455c09471ae8ecc9c29185d64636a77
Benson Peng <penglingzhe@gmail.com>
2015-04-26 12:37:54 +0800

    add voice cache manager

---
+++ iOSClient/Magic/Magic/Chat/Model/VoiceCacheManager.h
@@ -0,0 +1,15 @@
+//
+//  VoiceCacheManager.h
+//  Magic
+//
+//  Created by pipi on 15/4/26.
+//  Copyright (c) 2015年 PIPICHENG. All rights reserved.
+//
+
+#import "CommonManager.h"
+
+@interface VoiceCacheManager : CommonManager
+
+DEF_SINGLETON_FOR_CLASS(VoiceCacheManager);
+
+@end
---
+++ iOSClient/Magic/Magic/Chat/Model/VoiceCacheManager.m
@@ -0,0 +1,16 @@
+//
+//  VoiceCacheManager.m
+//  Magic
+//
+//  Created by pipi on 15/4/26.
+//  Copyright (c) 2015年 PIPICHENG. All rights reserved.
+//
+
+#import "VoiceCacheManager.h"
+#import "TMDiskCache.h"
+
+@implementation VoiceCacheManager
+
+IMPL_SINGLETON_FOR_CLASS(VoiceCacheManager);
+
+@end
